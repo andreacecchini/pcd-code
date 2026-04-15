59b5d87a5abe4e77c798af4b9a51d236160f4992
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-04-15 16:31:26 +0200

    added scala

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     id("java")
+    id("scala")
 }
 
 group = "it.unibo"
 version = "1.0-SNAPSHOT"
+
+val scalaVersion = "3.5.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.scala-lang:scala3-library_3:$scalaVersion")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
