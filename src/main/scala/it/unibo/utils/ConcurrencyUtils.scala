5fc9064e3474ae1b2eb3d613a93a268892f8eae2
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-04-16 10:33:29 +0200

    feat: lockable

--- src/main/scala/it/unibo/utils/ConcurrencyUtils.scala
+++ src/main/scala/it/unibo/utils/ConcurrencyUtils.scala
@@ -3,10 +3,19 @@ package it.unibo.utils
 import java.util.concurrent.locks.ReentrantLock
 
 object ConcurrencyUtils:
+  trait Lockable[L]:
+    def acquire(): Unit
+    def release(): Unit
+
+  given reentrantLockLockable(using lock: ReentrantLock): Lockable[ReentrantLock] with
+    override def acquire(): Unit = lock.lock()
+    override def release(): Unit = lock.unlock()
+
   def loop(body: => Unit): Unit = while true do body
-  def criticalSection[T](using lock: ReentrantLock)(body: => T): T =
-    lock.lock()
+  def criticalSection[L: Lockable, T](body: => T): T =
+    summon[Lockable[L]].acquire()
     try
       body
     finally
-      lock.unlock()
+      summon[Lockable[L]].release()
+  def work(millis: Int): Unit = Thread.sleep(millis)
